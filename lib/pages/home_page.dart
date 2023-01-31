c087afe6ef38d21e2acb7813745de193597bff6f
Sergio Garrido <sergarridom8@gmail.com>
2023-01-31 12:29:53 +0100

    Google Maps first geolocation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,8 @@
-import 'package:chat_app/helper/helper_functions.dart';
-import 'package:chat_app/model/chat_contact.dart';
-import 'package:chat_app/pages/chat_page.dart';
-import 'package:chat_app/pages/contacts_page.dart';
 import 'package:chat_app/pages/map_page.dart';
 import 'package:chat_app/pages/my_chats_page.dart';
 import 'package:chat_app/pages/profile_page.dart';
-import 'package:chat_app/screens/login_screen.dart';
 import 'package:chat_app/services/auth_service.dart';
-import 'package:chat_app/services/chat_service.dart';
-import 'package:chat_app/services/db_service.dart';
-import 'package:chat_app/widgets/widgets.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:chat_app/screens/login_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -24,13 +13,13 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   AuthService authService = AuthService();
-
   Stream? chats;
   int currentIndex = 0;
+
   List<Widget> pages = [
     const MyChatsPage(),
+    const MapPage(),
     const ProfilePage(),
-    const MapPage()
   ];
 
   void _onItemTapped(int index) {
@@ -46,12 +35,18 @@ class _HomePageState extends State<HomePage> {
           currentIndex: currentIndex,
           selectedItemColor: Theme.of(context).primaryColor,
           onTap: _onItemTapped,
+          backgroundColor: Colors.white,
+          elevation: 0,
           items: const [
             BottomNavigationBarItem(
                 icon: Icon(Icons.chat_rounded), label: "Mis chats"),
             BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: "Mapa",
+              backgroundColor: Colors.red,
+            ),
+            BottomNavigationBarItem(
                 icon: Icon(Icons.person), label: "Mi perfil"),
-            BottomNavigationBarItem(icon: Icon(Icons.map), label: "Mapa"),
           ],
         ),
         body: pages.elementAt(currentIndex));
--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:chat_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
@@ -11,19 +13,137 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high,
+    distanceFilter: 100,
+  );
+
+  StreamSubscription<Position>? positionStream;
+
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
+
+  final _markers = <Marker>[];
+  static const CameraPosition initialCameraPosition =
+      CameraPosition(target: LatLng(38.9842, -3.9275), zoom: 5);
+
+  var _finishedLocation = false;
+
+  Position? _currentPosition;
+
+  @override
+  void dispose() {
+    super.dispose();
+    // positionStream!.cancel();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() {
+    getCurrentPosition().then((value) async {
+      _markers.add(Marker(
+        markerId: const MarkerId("currentPosition"),
+        position: LatLng(value.latitude, value.longitude),
+      ));
+      CameraPosition cameraPosition = CameraPosition(
+          target: LatLng(value.latitude, value.longitude), zoom: 18);
+
+      final GoogleMapController controller = await _controller.future;
+
+      controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+      _finishedLocation = true;
+      setState(() {
+        _currentPosition = value;
+      });
+    });
+  }
+
+  Future<Position> getCurrentPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: GoogleMap(
-      mapType: MapType.normal,
-      initialCameraPosition: const CameraPosition(
-          target: LatLng(37.42796133580664, -122.085749655962), zoom: 14.4746),
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
+        body: Stack(
+      children: [
+        GoogleMap(
+          initialCameraPosition: initialCameraPosition,
+          markers: Set<Marker>.of(_markers),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+        if (!_finishedLocation) const Geolocating(),
+      ],
     ));
+  }
+}
+
+class Geolocating extends StatelessWidget {
+  const Geolocating({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 40),
+        child: Container(
+            height: 50,
+            constraints: BoxConstraints(
+                maxWidth: MediaQuery.of(context).size.width * 0.55),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color.fromARGB(76, 130, 130, 130),
+                    spreadRadius: 2,
+                    blurRadius: 2,
+                    offset: Offset(1, 1), // changes position of shadow
+                  ),
+                ],
+                borderRadius: BorderRadius.circular(30)),
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Loader(),
+                  SizedBox(width: 15),
+                  Text("Geolocalizando...", style: TextStyle(fontSize: 16))
+                ],
+              ),
+            )),
+      ),
+    );
   }
 }
