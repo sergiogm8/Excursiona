49a95aeb863dd871484eadf02e5e46e34dc06742
Sergio Garrido <sergarridom8@gmail.com>
2023-03-06 11:51:37 +0100

    Added more custom colors

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static const Color primaryColor = Color(0xFFFF6262);
+  static const Color indigoDye = Color.fromRGBO(0, 76, 109, 1);
+  static const Color lapisLazuli = Color.fromRGBO(5, 102, 141, 1);
+  static const Color steelBlue = Color.fromARGB(255, 75, 137, 182);
   static const Color secondaryColor = Color(0xFFB44444);
+
   static const Color lightChatColor = Color.fromARGB(255, 255, 210, 210);
   static const Color darkGrey = Color(0xFF757575);
   static const Color lightGrey = Color(0xFFE0E0E0);
