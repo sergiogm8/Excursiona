c6a9174d95bb16a5925787c17f2f5bc498a15a74
Sergio Garrido <sergarridom8@gmail.com>
2023-03-06 11:49:50 +0100

    Added assets resources file

---
+++ lib/constants/assets.dart
@@ -0,0 +1,17 @@
+class Assets {
+  Assets._();
+
+  /// Assets for resourceImagesLoginImage
+  /// assets/images/login_image.png
+  static const String resourceImagesLoginImage =
+      "assets/images/login_image.png";
+
+  /// Assets for resourceImagesLoginImage2
+  /// assets/images/login_image2.png
+  static const String resourceImagesLoginImage2 =
+      "assets/images/login_image2.png";
+
+  /// Assets for resourceImagesLogo
+  /// assets/images/logo.svg
+  static const String resourceImagesLogo = "assets/images/logo.svg";
+}
