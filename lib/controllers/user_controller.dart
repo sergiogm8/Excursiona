efefe023ec08a9fff681d361f8b20670d860fca2
Sergio Garrido <sergarridom8@gmail.com>
2023-03-22 13:26:36 +0100

    Basic UserController

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -1,7 +1,17 @@
+import 'package:excursiona/helper/helper_functions.dart';
+import 'package:excursiona/model/user_model.dart';
 import 'package:excursiona/services/user_service.dart';
 
 class UserController {
   final UserService _userService = UserService();
 
-  UserController();
+  Future<UserModel> getUserBasicInfo() async {
+    var name = await HelperFunctions.getUserName();
+    var profilePic = await HelperFunctions.getUserProfilePic();
+    return UserModel(name: name!, profilePic: profilePic!);
+  }
+
+  Stream<List<UserModel>> getAllUsersBasicInfo() {
+    return _userService.getAllUsersBasicInfo();
+  }
 }
