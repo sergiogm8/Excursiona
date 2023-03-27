371594dd931fb30d1cb2332f26d9487b0f8962a0
Sergio Garrido <sergarridom8@gmail.com>
2023-03-27 17:39:35 +0200

    Fetching and showing users

--- lib/pages/search_participants_page.dart
+++ lib/pages/search_participants_page.dart
@@ -1,5 +1,7 @@
+import 'package:excursiona/controllers/user_controller.dart';
 import 'package:excursiona/model/user_model.dart';
 import 'package:excursiona/shared/constants.dart';
+import 'package:excursiona/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -12,88 +14,98 @@ class SearchParticipantsPage extends StatefulWidget {
 }
 
 class _SearchParticipantsPageState extends State<SearchParticipantsPage> {
-  List<UserModel> _participants = [];
+  final TextEditingController _textController = TextEditingController();
+  final UserController _userController = UserController();
+  bool _isLoading = false;
+
   List<UserModel> _searchResults = [];
+  List<UserModel> _participants = [];
+
+  _fetchUsers() async {
+    var results = await _userController
+        .getAllUsersBasicInfo(_textController.text.toLowerCase());
+    setState(() {
+      _searchResults = results;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = true;
+    _fetchUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
+        appBar: AppBar(
+          title: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              // color: Colors.grey[200],
+              color: Constants.aliceBlue,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
               children: [
-                const SizedBox(height: 38),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Row(
-                    children: [
-                      const Icon(Icons.search),
-                      const SizedBox(width: 16),
-                      const Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Buscar",
-                          ),
-                        ),
-                      ),
-                      TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "Cancelar",
-                            style: TextStyle(color: Constants.indigoDye),
-                          ))
-                    ],
+                const Icon(
+                  Icons.search,
+                  color: Constants.indigoDye,
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Buscar",
+                    ),
+                    cursorColor: Constants.indigoDye,
+                    controller: _textController,
+                    onChanged: (value) {
+                      _fetchUsers();
+                    },
                   ),
                 ),
-                const Text(
-                  "Buscar participantes",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  "Busca a tus amigos y añádelos a la excursión",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const SizedBox(height: 16),
-                const Text(
-                  "Resultados",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const Text(
-                  "No se han encontrado resultados",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.grey,
-                  ),
-                ),
-                ElevatedButton(
-                    onPressed: () =>
-                        Navigator.pop(context, _participants.length),
-                    child: const Text("Añadir participante")),
-                ElevatedButton(onPressed: () => {}, child: const Text("+")),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Cancelar",
+                      style: TextStyle(color: Constants.indigoDye),
+                    )),
               ],
             ),
           ),
+          backgroundColor: const Color(0xFFFAFAFA),
+          automaticallyImplyLeading: false,
+          elevation: 0,
         ),
-      ),
-    );
+        body: _isLoading
+            ? const Loader()
+            : ListView.separated(
+                itemBuilder: (context, index) {
+                  return ListTile(
+                      title: Text(_searchResults[index].name),
+                      leading: _searchResults[index].profilePic.isEmpty
+                          ? const Icon(Icons.account_circle, size: 60)
+                          : CircleAvatar(
+                              radius: 30,
+                              backgroundImage: NetworkImage(
+                                  _searchResults[index].profilePic)),
+                      trailing: TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _participants.add(_searchResults[index]);
+                            });
+                          },
+                          child: const Text("Añadir")));
+                },
+                separatorBuilder: (context, index) {
+                  return const Divider();
+                },
+                itemCount: _searchResults.length));
   }
 }
