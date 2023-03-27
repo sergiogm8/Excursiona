ca116dc2e89dc6bdb228c4ec335f88d06b6c7d17
Sergio Garrido <sergarridom8@gmail.com>
2023-03-27 13:03:56 +0200

    Added AliceBlue color

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -5,6 +5,7 @@ class Constants {
   static const Color lapisLazuli = Color(0xFF05668D);
   static const Color steelBlue = Color(0xFF4B89B6);
   static const Color secondaryColor = Color(0xFFB44444);
+  static const Color aliceBlue = Color(0xFFEBF2FA);
 
   static const Color lightChatColor = Color.fromARGB(255, 255, 210, 210);
   static const Color darkGrey = Color(0xFF757575);
