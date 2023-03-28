e163d57475172f5210498939327361e29f4b339f
Sergio Garrido <sergarridom8@gmail.com>
2023-03-28 17:38:38 +0200

    Added search participants page

--- lib/pages/search_participants_page.dart
+++ lib/pages/search_participants_page.dart
@@ -1,6 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:excursiona/controllers/user_controller.dart';
+import 'package:excursiona/helper/helper_functions.dart';
 import 'package:excursiona/model/user_model.dart';
 import 'package:excursiona/shared/constants.dart';
+import 'package:excursiona/shared/utils.dart';
 import 'package:excursiona/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -14,21 +17,11 @@ class SearchParticipantsPage extends StatefulWidget {
 }
 
 class _SearchParticipantsPageState extends State<SearchParticipantsPage> {
+  bool _isLoading = false;
+  final Set<UserModel> _participants = {};
+  List<UserModel> _searchResults = [];
   final TextEditingController _textController = TextEditingController();
   final UserController _userController = UserController();
-  bool _isLoading = false;
-
-  List<UserModel> _searchResults = [];
-  List<UserModel> _participants = [];
-
-  _fetchUsers() async {
-    var results = await _userController
-        .getAllUsersBasicInfo(_textController.text.toLowerCase());
-    setState(() {
-      _searchResults = results;
-      _isLoading = false;
-    });
-  }
 
   @override
   void initState() {
@@ -37,75 +30,118 @@ class _SearchParticipantsPageState extends State<SearchParticipantsPage> {
     _fetchUsers();
   }
 
+  _fetchUsers() async {
+    var results = await _userController
+        .getAllUsersBasicInfo(_textController.text.toLowerCase());
+    var uid = await HelperFunctions.getUserUID();
+    setState(() {
+      _searchResults = results.where((element) => element.uid != uid).toList();
+      _isLoading = false;
+    });
+  }
+
+  _addParticipant(UserModel user) {
+    bool isParticipant = _participants.contains(user);
+    if (!isParticipant) {
+      setState(() {
+        _participants.add(user);
+      });
+      showSnackBar(context, Colors.green,
+          "Se agregó a ${user.name} a la lista de participantes");
+    } else {
+      showSnackBar(context, Constants.indigoDye,
+          "El usuario ${user.name} ya está en la lista de participantes");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            decoration: BoxDecoration(
-              // color: Colors.grey[200],
-              color: Constants.aliceBlue,
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Row(
-              children: [
-                const Icon(
-                  Icons.search,
-                  color: Constants.indigoDye,
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Buscar",
-                    ),
-                    cursorColor: Constants.indigoDye,
-                    controller: _textController,
-                    onChanged: (value) {
-                      _fetchUsers();
-                    },
-                  ),
-                ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text(
-                      "Cancelar",
-                      style: TextStyle(color: Constants.indigoDye),
-                    )),
-              ],
-            ),
+      appBar: AppBar(
+        title: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          decoration: BoxDecoration(
+            // color: Colors.grey[200],
+            color: Constants.aliceBlue,
+            borderRadius: BorderRadius.circular(30),
           ),
-          backgroundColor: const Color(0xFFFAFAFA),
-          automaticallyImplyLeading: false,
-          elevation: 0,
+          child: Row(
+            children: [
+              const Icon(
+                Icons.search,
+                color: Constants.indigoDye,
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: TextField(
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Buscar",
+                  ),
+                  cursorColor: Constants.indigoDye,
+                  controller: _textController,
+                  onChanged: (value) {
+                    _fetchUsers();
+                  },
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  "Cancelar",
+                  style: TextStyle(color: Constants.indigoDye),
+                ),
+              ),
+            ],
+          ),
         ),
-        body: _isLoading
+        backgroundColor: const Color(0xFFFAFAFA),
+        // automaticallyImplyLeading: false,
+        leadingWidth: 30,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          color: Constants.indigoDye,
+          onPressed: () {
+            Navigator.pop(context, _participants);
+          },
+        ),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: _isLoading
             ? const Loader()
             : ListView.separated(
                 itemBuilder: (context, index) {
                   return ListTile(
                       title: Text(_searchResults[index].name),
                       leading: _searchResults[index].profilePic.isEmpty
-                          ? const Icon(Icons.account_circle, size: 60)
+                          ? const CircleAvatar(
+                              foregroundColor: Colors.grey,
+                              backgroundColor: Colors.transparent,
+                              radius: 30,
+                              child: Icon(
+                                Icons.account_circle,
+                                size: 60,
+                              ))
                           : CircleAvatar(
                               radius: 30,
-                              backgroundImage: NetworkImage(
-                                  _searchResults[index].profilePic)),
+                              backgroundImage: CachedNetworkImageProvider(
+                                  _searchResults[index].profilePic),
+                            ),
                       trailing: TextButton(
-                          onPressed: () {
-                            setState(() {
-                              _participants.add(_searchResults[index]);
-                            });
-                          },
-                          child: const Text("Añadir")));
+                          onPressed: () =>
+                              _addParticipant(_searchResults[index]),
+                          child: const Text("Añadir",
+                              style: TextStyle(color: Constants.indigoDye))));
                 },
                 separatorBuilder: (context, index) {
                   return const Divider();
                 },
-                itemCount: _searchResults.length));
+                itemCount: _searchResults.length),
+      ),
+    );
   }
 }
