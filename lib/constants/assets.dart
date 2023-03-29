b240c428aef8156a1d56e15e8371a1cc6b4c62d5
Sergio Garrido <sergarridom8@gmail.com>
2023-03-29 13:41:01 +0200

    Loading GIF

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -1,13 +1,15 @@
 class Assets {
   Assets._();
-
+  
   /// Assets for resourceImagesLoginImage
   /// assets/images/login_image.png
-  static const String resourceImagesLoginImage =
-      "assets/images/login_image.png";
+  static const String resourceImagesLoginImage = "assets/images/login_image.png";
 
   /// Assets for resourceImagesLogoRecortado
   /// assets/images/logo_recortado.png
-  static const String resourceImagesLogoRecortado =
-      "assets/images/logo_recortado.png";
+  static const String resourceImagesLogoRecortado = "assets/images/logo_recortado.png";
+
+  /// Assets for resourceImagesMaploader
+  /// assets/images/maploader.gif
+  static const String resourceImagesMaploader = "assets/images/maploader.gif";
 }
