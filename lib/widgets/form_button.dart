363b0e1bd9c37119e229579982b5fd436a43b079
Sergio Garrido <sergarridom8@gmail.com>
2023-03-29 11:45:43 +0200

    Added optional icon for FormButton

--- lib/widgets/form_button.dart
+++ lib/widgets/form_button.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 
 class FormButton extends StatelessWidget {
-  const FormButton({super.key, required this.text, required this.onPressed});
+  const FormButton(
+      {super.key, required this.text, required this.onPressed, this.icon});
 
+  final IconData? icon;
   final onPressed;
   final String text;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
-        onPressed();
-      },
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Theme.of(context).primaryColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
+        onPressed: () {
+          onPressed();
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Theme.of(context).primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          fixedSize: Size(MediaQuery.of(context).size.width, 50),
         ),
-        fixedSize: Size(MediaQuery.of(context).size.width, 50),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-    );
+        child: icon == null
+            ? Text(
+                text,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    text,
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Icon(icon, size: 30),
+                ],
+              ));
   }
 }
