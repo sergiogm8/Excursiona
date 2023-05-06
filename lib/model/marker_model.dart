7b87edfbce121c7a371319f2fb5e64892787661d
Sergio Garrido <sergarridom8@gmail.com>
2023-05-06 12:26:21 +0200

    Added MarkerModel

---
+++ lib/model/marker_model.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:excursiona/enums/marker_type.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MarkerModel {
+  final String id;
+  final String? title;
+  final String? imageUrl;
+  final String? ownerName;
+  final String? ownerPic;
+  final String userId;
+  final LatLng position;
+  final MarkerType markerType;
+
+  MarkerModel(
+      {required this.id,
+      required this.position,
+      required this.markerType,
+      required this.userId,
+      this.title,
+      this.imageUrl,
+      this.ownerName,
+      this.ownerPic});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'imageUrl': imageUrl,
+      'position': GeoPoint(position.latitude, position.longitude),
+      'markerType': markerType.type,
+      'ownerName': ownerName,
+      'ownerPic': ownerPic,
+      'userId': userId,
+    };
+  }
+
+  factory MarkerModel.fromMap(Map<String, dynamic> map) {
+    return MarkerModel(
+        id: map['id'] as String,
+        userId: map['userId'],
+        ownerName: map['ownerName'] as String?,
+        ownerPic: map['ownerPic'] as String?,
+        title: map['title'] as String?,
+        imageUrl: map['imageUrl'] as String?,
+        position: LatLng(map['position'].latitude, map['position'].longitude),
+        markerType: MarkerType.fromString(map['markerType']));
+  }
+}
