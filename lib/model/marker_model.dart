8fe9b0d3feb5a16c0394a018c115cffb8600820c
Sergio Garrido <sergarridom8@gmail.com>
2023-05-10 13:58:02 +0200

    Updated MarkerModel with new params

--- lib/model/marker_model.dart
+++ lib/model/marker_model.dart
@@ -5,51 +5,68 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MarkerModel {
   final String id;
-  final String? title;
-  final String? imageUrl;
-  final String? ownerName;
-  final String? ownerPic;
   final String userId;
   final LatLng position;
   final MarkerType markerType;
   final DateTime timestamp;
+  final String? title;
+  final String? imageUrl;
+  final String? ownerName;
+  final String? ownerPic;
+  final double? altitude;
+  final double? speed;
+  final double? distance;
+  final int? batteryLevel;
 
   MarkerModel({
     required this.id,
+    required this.userId,
     required this.position,
     required this.markerType,
-    required this.userId,
+    required this.timestamp,
     this.title,
     this.imageUrl,
     this.ownerName,
     this.ownerPic,
-    required this.timestamp,
+    this.altitude,
+    this.speed,
+    this.distance,
+    this.batteryLevel,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'id': id,
+      'userId': userId,
+      'position': GeoPoint(position.latitude, position.longitude),
       'title': title,
       'imageUrl': imageUrl,
-      'position': GeoPoint(position.latitude, position.longitude),
       'markerType': markerType.type,
       'ownerName': ownerName,
       'ownerPic': ownerPic,
-      'userId': userId,
-      'timestamp': timestamp.millisecondsSinceEpoch
+      'timestamp': timestamp.millisecondsSinceEpoch,
+      'altitude': altitude,
+      'speed': speed,
+      'distance': distance,
+      'batteryLevel': batteryLevel,
     };
   }
 
   factory MarkerModel.fromMap(Map<String, dynamic> map) {
     return MarkerModel(
-        id: map['id'] as String,
-        userId: map['userId'],
-        ownerName: map['ownerName'] as String?,
-        ownerPic: map['ownerPic'] as String?,
-        title: map['title'] as String?,
-        imageUrl: map['imageUrl'] as String?,
-        position: LatLng(map['position'].latitude, map['position'].longitude),
-        markerType: MarkerType.fromString(map['markerType']),
-        timestamp: DateTime.fromMillisecondsSinceEpoch(map['timestamp']));
+      id: map['id'] as String,
+      userId: map['userId'],
+      ownerName: map['ownerName'] as String?,
+      ownerPic: map['ownerPic'] as String?,
+      title: map['title'] as String?,
+      imageUrl: map['imageUrl'] as String?,
+      position: LatLng(map['position'].latitude, map['position'].longitude),
+      altitude: map['altitude'] as double?,
+      markerType: MarkerType.fromString(map['markerType']),
+      timestamp: DateTime.fromMillisecondsSinceEpoch(map['timestamp']),
+      speed: map['speed'] as double?,
+      distance: map['distance'] as double?,
+      batteryLevel: map['batteryLevel'] as int?,
+    );
   }
 }
